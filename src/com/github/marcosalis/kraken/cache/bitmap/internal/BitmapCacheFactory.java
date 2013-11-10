e4c12131b7c623dd1cd7cac8e12325221de4fb4b
Marco Salis <fast3r@gmail.com>
2013-11-10 19:57:45 +0000

    BitmapCacheFactory: internal factory class to build BitmapCaches

---
+++ src/com/github/marcosalis/kraken/cache/bitmap/internal/BitmapCacheFactory.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2013 Marco Salis - fast3r@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.marcosalis.kraken.cache.bitmap.internal;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.github.marcosalis.kraken.cache.bitmap.BitmapCache;
+import com.github.marcosalis.kraken.cache.bitmap.BitmapDiskCache;
+import com.github.marcosalis.kraken.cache.bitmap.BitmapMemoryCache;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.common.annotations.Beta;
+
+/**
+ * Internal utility class containing static methods to build the default
+ * implementations of {@link BitmapCache}s and their components.
+ * 
+ * @since 1.0
+ * @author Marco Salis
+ */
+@Beta
+public class BitmapCacheFactory {
+
+	private BitmapCacheFactory() {
+		// no instantiation needed
+	}
+
+	/**
+	 * Builds an instance of the default {@link BitmapCache} implementation
+	 * given the passed components.
+	 */
+	@Nonnull
+	public static BitmapCache buildDefaultBitmapCache(@Nonnull BitmapMemoryCache<String> cache,
+			@Nullable BitmapDiskCache diskCache, @Nonnull HttpRequestFactory factory) {
+		return new BitmapCacheImpl(cache, diskCache, factory);
+	}
+
+}
