cacbcf23187dd7248e92d12d77554f7a5b48ab02
Marco Salis <fast3r@gmail.com>
2013-11-15 12:18:27 +0000

    License: masked email address in all source files

--- kraken_tests/src/com/github/marcosalis/kraken/testing/framework/TestAssertsWrapper.java
+++ kraken_tests/src/com/github/marcosalis/kraken/testing/framework/TestAssertsWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Marco Salis - fast3r@gmail.com
+ * Copyright 2013 Marco Salis - fast3r(at)gmail.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
